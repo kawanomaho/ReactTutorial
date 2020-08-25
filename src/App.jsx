d19f1d54740e47d24288e27f2361ca0d4f7689ef
root <root@KNM075.localdomain>
2020-08-25 15:40:55 +0900

    Hooksに書き換え

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,80 @@
-import React from "react"
-import Board from "./Board"
+import React, { useState } from "react";
+import Board from "./Board";
 
-export default class App extends React.Component {
-  render() {
+export default function App(props) {
+  const [histories, setHistories] = useState([
+    { squares: Array(9).fill(null) },
+  ]);
+  const [isX, setIsX] = useState(false);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const handleClick = (i) => {
+    const history = histories.slice(0, stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = isX ? "X" : "O";
+    setHistories(history.concat([{ squares: squares }]));
+    setStepNumber(history.length);
+    setIsX(!isX);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setIsX(step % 2 === 0);
+  };
+
+  const current = histories[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = histories.map((step, move) => {
+    const desc = move ? `${move} に移動する。` : "ゲームを始める。";
+    const handleJumpTo = () => {
+      jumpTo(move);
+    };
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* todo */}</ol>
-        </div>
-      </div>
-    )
+      <li key={move}>
+        <button onClick={handleJumpTo}>{desc}</button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner) {
+    status = "勝者: " + winner;
+  } else {
+    status = "次のプレイヤー: " + (isX ? "X" : "O");
   }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
--- src/Board.jsx
+++ src/Board.jsx
@@ -1,29 +1,35 @@
-import React from "react"
-import Square from "./Square"
+import React from "react";
+import Square from "./Square";
 
-export default function(props) {
-  const renderSquare = i => {
-    return <Square />
+export default class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
-  const status = "次のプレイヤー: X"
-  return (
-    <div>
-      <div>{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  )
+    );
+  }
 }
